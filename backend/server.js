700fe32008c0e6165fb15d44bfe2de055df3f0d8
AsthaPitambarwale <asthapitambarwale@gmail.com>
2026-05-06 22:32:20 +0530

    Intial

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://",
+      "https://doodle-delight-india-s-best-statine.vercel.app/",
     ],
     credentials: true,
   }),
